15e5bea9ad83dea80a83e6f0e0bbc493d21073be
Max <maxhager@ziggo.nl>
2023-06-06 22:26:00 +0200

    Updated to Java 17 and added libraries

---
+++ src/RoboticArm.java
@@ -0,0 +1,16 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+import org.jfree.fx.FXGraphics2D;
+
+public class RoboticArm extends Application {
+
+    public static void main(String[] args) {
+        launch(RoboticArm.class);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        System.out.println("Test!");
+    }
+
+}
